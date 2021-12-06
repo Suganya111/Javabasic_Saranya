a66920f957b4f8d8c3c0be68a93e34fa90e0dd75
unknown <saranyaganesana@gmail.com>
2021-12-06 21:17:27 +0530

    Forloop

---
+++ src/com/aaludrajavabasicprograms/Forexample.java
@@ -0,0 +1,13 @@
+
+	package com.aaludrajavabasicprograms;
+public class Forexample{
+	public static void main(String args[]) {
+		int i;
+			for(i=1 ;i<=10;i++) {
+				System.out.println("the value of i is" +i);
+			}
+	}
+}
+	 
+		
+	
