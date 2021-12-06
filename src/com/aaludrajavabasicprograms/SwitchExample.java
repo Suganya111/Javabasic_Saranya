448cc42782dd7c1ad0a82122415434c5f8091ff1
unknown <saranyaganesana@gmail.com>
2021-12-06 17:21:16 +0530

    Switch

---
+++ src/com/aaludrajavabasicprograms/SwitchExample.java
@@ -0,0 +1,30 @@
+package com.aaludrajavabasicprograms;
+
+public class SwitchExample {
+	public static void main(String args[]) {
+		int month=4;
+		String monthstring;
+		switch(month) {
+		case 1:
+			System.out.println("January");
+			break;
+		case 2:
+			System.out.println("Febraury");
+			break;
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+			break;
+		default:
+			System.out.println("invalid");
+		}
+			
+		
+	}
+
+}
