e30596b959f4b419e3bfe34d3688ca9bc662cd2a
unknown <saranyaganesana@gmail.com>
2021-12-06 16:59:07 +0530

    Ifelse

---
+++ src/com/aaludrajavabasicprograms/Ifelseexample.java
@@ -0,0 +1,17 @@
+package com.aaludrajavabasicprograms;
+
+public class Ifelseexample {
+	public static void main(String args[]) {
+		int age=20;
+		if(age>31) {
+			System.out.println("age is greater than 20");
+		}
+		else if(age==20) {
+			System.out.println("age is 20");
+		}
+		else {
+			System.out.println("age is less than 20");
+		}
+	}
+
+}
