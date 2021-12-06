8752e6fc2e437cea87ee60ab66b2c959bcfcaead
unknown <saranyaganesana@gmail.com>
2021-12-06 18:04:06 +0530

    While

---
+++ src/com/aaludrajavabasicprograms/Whileexample.java
@@ -0,0 +1,14 @@
+package com.aaludrajavabasicprograms;
+
+public class Whileexample {
+	public static void main(String args[]) {
+		int i=1; 
+		int n=5;
+		while(i<=n) {
+			System.out.println(i);
+			i++;
+			
+		}
+	}
+
+}
