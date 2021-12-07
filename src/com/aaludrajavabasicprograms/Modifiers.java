2bbb96db9877830d070abccd05f4e3f2afa31674
unknown <saranyaganesana@gmail.com>
2021-12-07 17:08:30 +0530

    Class

---
+++ src/com/aaludrajavabasicprograms/Modifiers.java
@@ -0,0 +1,19 @@
+package com.aaludrajavabasicprograms;
+
+public class Modifiers {
+private int empsalary =200; // can be access only in this class
+public String empname="Saranya";// can accessed anywhere in the class and package
+protected int empid = 41;//can be accessed anywhere in the same package and also access able
+// if only if it is a sub class of other package class
+
+public void employeedetails() {
+	System.out.println("employee details");
+	System.out.println("employee name:" +empname);
+	System.out.println("empid:" +empid);
+    System.out.println("empsalary:" +empsalary);
+}
+public static void main(String args[]) {
+	Modifiers obj=new Modifiers();
+	obj.employeedetails();
+}
+}
