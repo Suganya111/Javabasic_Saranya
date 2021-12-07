8ce78a0ab8b0f6a75bd63a611a023bc272c18e03
unknown <saranyaganesana@gmail.com>
2021-12-07 13:00:15 +0530

    Method

--- src/com/aaludrajavabasicprograms/Methodexample.java
+++ src/com/aaludrajavabasicprograms/Methodexample.java
@@ -1,20 +1,17 @@
 package com.aaludrajavabasicprograms;
 
-public class Methodexample {
-   public int addNumber(int a, int b) {
-	   int sum=a+b;
-	   return sum;
-   }
- public static void main(String args[])
- {
-	 int num1=20;
-	 int num2=15;
- Methodexample obj=new Methodexample();
- int result=obj.addNumber(num1,num2);
- System.out.println("sum is:" +result);
- }
-}
-	 
-	 
-	 
- 
+public class MethodExample {
+	public int addNumber(int a, int b) {
+			   int sum=a+b;
+			   return sum;
+		   }
+		 public static void main(String args[])
+		 {
+			 int num1=20;
+			 int num2=15;
+		 MethodExample obj=new MethodExample();
+		 int result=obj.addNumber(num1,num2);
+		 System.out.println("sum is:" +result);
+		 }
+		}
+
