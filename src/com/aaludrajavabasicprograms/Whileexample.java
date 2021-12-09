bee886098b04cd0a5e9143ff7b2555c9f6bead2f
unknown <saranyaganesana@gmail.com>
2021-12-09 12:17:56 +0530

    While

--- src/com/aaludrajavabasicprograms/Whileexample.java
+++ src/com/aaludrajavabasicprograms/Whileexample.java
@@ -2,16 +2,13 @@ package com.aaludrajavabasicprograms;
 
 public class Whileexample {
 	public static void main(String args[]) {
-		int i,j,n=1;
-		i=10;
-		while(i>=n) {
-			j=10;
-			while(j>=i) {
-				System.out.print(i);
-				j--;
-			}
-			System.out.println(" ");
+		
+		String [] myarr= {"a","y","n","a","r","a","s"};
+	int i=myarr.length-1;
+	while(i>=0) {
+				System.out.print(myarr[i]);
 			i--;
+			
 			}
 		}
 }
